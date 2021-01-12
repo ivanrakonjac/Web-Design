3a5e10dfcfb0b903f59021401f07ed56676075b6
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2021-01-12 19:48:19 +0100

    .NET Core MessagingApp: Add CreateConversation method to ChatController

--- .NET Core/MessagingApp/Controllers/ChatController.cs
+++ .NET Core/MessagingApp/Controllers/ChatController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -42,6 +43,79 @@ namespace MessagingApp.Controllers{
             };
 
             return View ( model );
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateConversation (CreateConversationModel model){
+            if( !ModelState.IsValid ){
+                return View ( model );
+            }
+
+            String name = model.name;
+
+            // Proveramo ima li i jednog selectovanog usera
+            bool userSelected = model.userSelectionsList.Select ( item => item.selected ).Aggregate (
+                (result, item) => result | item
+            );
+
+            if( !userSelected ){
+                ModelState.AddModelError ( "", "No user selected!" );
+                return View (model);
+            }
+
+
+            /* Provera da li vec postoji cet sa selectovanim korisnicima */
+
+            // Dohvatam ulogovanog korisnika
+            User loggedInUser = await this.userManager.GetUserAsync (base.User);
+
+            // Dojvatam selectovane usere i appendujem id ulogovanog usera
+            string[] selectedIds = model.userSelectionsList.Where (item => item.selected == true)
+                                    .Select (item => item.id)
+                                    .Append (loggedInUser.Id)
+                                    .ToArray ( );
+
+            bool conversationExists = this.context.userConversations
+                                        // da li se item.userId nalazi u kolekciji selektovanih korisnika
+                                        .Where (item => selectedIds.Contains (item.userId) )
+                                        // grupisem userConversations tabelu po razgovorima
+                                        .GroupBy ( item => item.conversationId )
+                                        // iz rezultata izvlacim za svaku grupu broj redova sa datom grupom
+                                        .Select ( group => new { group.Key, count = group.Count () })
+                                        // trebaju mi samo razgovori sa onoliko korisnika koliko je selectovano idjeva
+                                        .Where ( item => item.count == selectedIds.Count ( ) )
+                                        // postoji li koji takav
+                                        .Any ( );
+
+            if(conversationExists) {
+                ModelState.AddModelError ("", "Selected users are already in conversations");
+                return View ( model );
+            }
+
+            /* Kraj provere da li vec postoji cet sa selectovanim korisnicima */
+
+            // pravim novu konverzaciju i inicijalizujem je potrebnim podacima
+            Conversation conversation = new Conversation  ( ){
+                name = model.name,
+                creationDate = DateTime.Now,
+                UserConversationList = new List<UserConversation> ( )
+            };
+
+
+            // dodajem idjeve u conversation.UserConversationList
+            foreach (string id in selectedIds) {
+                conversation.UserConversationList.Add (
+                    new UserConversation ( ){
+                        userId = id
+                    }
+                );
+            }
+
+            await this.context.conversations.AddAsync ( conversation );
+            await this.context.SaveChangesAsync ( );
+
+            return RedirectToAction ( nameof ( ChatController.Index ) );
         }
     }
 }
