5bbb0033bdb256b24e056a75e71eb8493c0546f6
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2021-01-13 13:08:14 +0100

    .NET Core MessagingApp: Add timer on Index page

--- .NET Core/MessagingApp/wwwroot/js/site.js
+++ .NET Core/MessagingApp/wwwroot/js/site.js
@@ -2,3 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function updateTimer ( ){
+    var string = $("#timer").text ( );
+
+    var array = string.split (":");
+
+    var hours = parseInt(array[0]);
+    var minutes = parseInt(array[1]);
+    var seconds = parseInt(array[2]);
+
+    var timeInSeconds = hours * 3600 + minutes * 60 + seconds + 1;
+
+    seconds = timeInSeconds % 60 ;
+    minutes = Math.floor ( timeInSeconds / 60 ) % 60; 
+    hours = Math.floor ( timeInSeconds / 3600 );
+    
+    if( seconds < 10){
+        seconds = "0" + seconds;
+    }
+
+    if( minutes < 10){
+        minutes = "0" + minutes;
+    }
+
+    if( hours < 10){
+        hours = "0" + hours;
+    }
+
+    $("#timer").text ( hours + ":" + minutes + ":" + seconds);
+}
+
+// JS funkcija koja zove update timer svakih 1000ms
+setInterval ( updateTimer, 1000 );
