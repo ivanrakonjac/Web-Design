746a2086dae211611f9576bb6a0e0b86dc3b9d2a
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2021-01-17 01:28:02 +0100

    .NET Core ImagePreview: Add Image model ; Initial DB migration

---
+++ .NET Core/ImagePreview/Migrations/20210117002258_Initial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ImagePreview.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "images",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    data = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_images", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "images");
+        }
+    }
+}
--- .NET Core/ImagePreview/Models/Image.cs
+++ .NET Core/ImagePreview/Models/Image.cs
@@ -1,5 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace ImagePreview.Models{
     public class Image {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public byte[] data { get; set; }
+    }
 
+    public class ImageConfiguration : IEntityTypeConfiguration<Image>
+    {
+        public void Configure(EntityTypeBuilder<Image> builder)
+        {
+            builder.Property ( image => image.id )
+                .ValueGeneratedOnAdd ( );
+        }
     }
 }
