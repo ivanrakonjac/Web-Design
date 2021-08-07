3ff01b032a09c7d84658ed909eafd90b5ccb39b0
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2021-08-07 16:54:05 +0200

    How to create connection to MongoDB

--- MEAN/Bookstore/backend/src/server.ts
+++ MEAN/Bookstore/backend/src/server.ts
@@ -1,4 +1,23 @@
 import express from 'express';
+import cors from 'cors'
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose'
+
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017/BookStoreDB");
+
+const conn = mongoose.connection;
+
+conn.once('open',()=>{
+    console.log('Uspesna konekcija')
+})
+
+const router = express.Router();
+
+
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(4000, () => console.log(`Express server running on port 4000`));
